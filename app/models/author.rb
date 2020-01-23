61145aec366a3118e9a22dee14a0515a84410b6e
Leonardo Prado <leonardoprado@gmail.com>
2020-01-23 17:05:26 -0300

    Improve Author validation and tests
    Check for nil on #name, and validate it's presence

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,12 @@
 class Author < ApplicationRecord
   validates :name, uniqueness: true
+  validates :name, presence: true
 
   def name=(value)
-    super value.squish
+    super value&.squish
   end
 
   def name
-    Name.new(super)
+    Name.new(super) if super.present?
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,7 +4,16 @@ RSpec.describe Author, type: :model do
   let(:name) { 'Leonardo Prado' }
 
   subject { described_class.new(name: name) }
-  
+
+  describe 'Validate name presence' do
+    let(:name) { nil }
+
+    before { subject.valid? }
+
+    it { expect(subject.valid?).to be_falsy }
+    it { expect(subject.errors.details).to include(name: [error: :blank]) }
+  end
+
   describe 'Uniqueness name validation' do
     before do
       described_class.create!(name: name)
@@ -12,7 +21,7 @@ RSpec.describe Author, type: :model do
     end
 
     it { expect(subject.valid?).to be_falsy }
-    it { expect(subject.errors.messages).to include(name: ['já está em uso']) }
+    it { expect(subject.errors.details).to include(name: [{ error: :taken, value: name }]) }
   end
 
   describe '#name' do
